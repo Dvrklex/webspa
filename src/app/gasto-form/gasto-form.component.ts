d1582f60349bb124844c4b444390e2891e3b2020
TadeoPariani <tadeopariani1@gmail.com>
2023-08-21 17:10:15 -0300

    Formulario

---
+++ src/app/gasto-form/gasto-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gasto-form',
+  templateUrl: './gasto-form.component.html',
+  styleUrls: ['./gasto-form.component.css']
+})
+export class GastoFormComponent {
+
+}
